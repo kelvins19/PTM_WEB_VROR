70674eed3ba498864053b85a66131259bb9b4ba5
kelvinsIAV <kelvin@itsavirus.com>
2022-05-02 16:52:54 +0800

    Adjust styling for rak page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,9 @@
 // Entry point for the build script in your package.json
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
